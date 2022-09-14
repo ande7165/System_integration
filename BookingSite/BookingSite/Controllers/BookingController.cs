52bef1ee53395c3a7ee5b19564cd3852ba2d8214
Anders Skov Jensen <ande7165@edu.ucl.dk>
2022-09-14 22:04:15 +0200

    Booking

---
+++ BookingSite/BookingSite/Controllers/BookingController.cs
@@ -0,0 +1,46 @@
+﻿using BookingSite.Model;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Linq;
+using RabbitMQ.Client;
+using System.Text.Json;
+using System.Diagnostics;
+
+namespace BookingSite.Controllers
+{
+	public class BookingController : Controller { 
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult SendMessage(Booking booking)
+		{
+			if(booking != null)
+			{
+				var factory = new ConnectionFactory() { HostName = "localhost" };
+				using (var connection = factory.CreateConnection())
+				using (var channel = connection.CreateModel())
+				{
+					channel.ExchangeDeclare(exchange: "topic_logs",
+											type: "topic");
+
+					var routingKey = "Booking.Message";
+					var message = JsonSerializer.Serialize(booking);
+					var body = Encoding.UTF8.GetBytes(message);
+					channel.BasicPublish(exchange: "topic_logs",
+										 routingKey: routingKey,
+										 basicProperties: null,
+										 body: body);
+
+					Debug.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+				}
+
+			}
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ BookingSite/BookingSite/Model/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace BookingSite.Model
+{
+	public class Booking
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Tour { get; set; }
+		public bool Book { get; set; }
+	}
+}
