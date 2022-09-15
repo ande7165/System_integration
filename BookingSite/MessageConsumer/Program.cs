76ca3d1c96cb55a0e8f8ec14a782acce65389d67
Anders Skov Jensen <ande7165@edu.ucl.dk>
2022-09-15 09:25:59 +0200

    RR Pattern

--- BookingSite/MessageConsumer/Program.cs
+++ BookingSite/MessageConsumer/Program.cs
@@ -5,28 +5,29 @@ using System.Text;
 var factory = new ConnectionFactory() { HostName = "localhost" };
 
 
-using (var connection = factory.CreateConnection()) { 
+using (var connection = factory.CreateConnection())
+{
 	using (var channel = connection.CreateModel())
 	{
 		channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
 		var queueName = channel.QueueDeclare().QueueName;
 
-		if (args.Length < 1)
-		{
-			Console.Error.WriteLine("Usage: {0} [binding_key...]",
-									Environment.GetCommandLineArgs()[0]);
-			Console.WriteLine(" Press [enter] to exit.");
-			Console.ReadLine();
-			Environment.ExitCode = 1;
-			return;
-		}
+		//if (args.Length < 1)
+		//{
+		//	Console.Error.WriteLine("Usage: {0} [binding_key...]",
+		//							Environment.GetCommandLineArgs()[0]);
+		//	Console.WriteLine(" Press [enter] to exit.");
+		//	Console.ReadLine();
+		//	Environment.ExitCode = 1;
+		//	return;
+		//}
 
-		foreach (var bindingKey in args)
-		{
-			channel.QueueBind(queue: queueName,
-							  exchange: "topic_logs",
-							  routingKey: bindingKey);
-		}
+		//foreach (var bindingKey in args)
+		//{
+		channel.QueueBind(queue: queueName,
+						  exchange: "topic_logs",
+						  routingKey: "Booking.Message");
+		//}
 
 		Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
 
